b5f3f78ade6b9207e0d649147fabda995b407cb6
Baltrock <bengmmurphy@hotmail.com>
2023-01-31 22:16:15 +0000

    please

--- app/controllers/admin/registrations_controller.rb
+++ app/controllers/admin/registrations_controller.rb
@@ -5,17 +5,17 @@ class Admin::RegistrationsController < Devise::RegistrationsController
 
   protected
 
-  def after_sign_in_path_for(resource)
-    if resource.admin
-      :admin
-    elsif resource.admin == false
-      :users
-    else
-      super
-    end
-  end
+  # def after_sign_in_path_for(resource)
+  #   if resource.admin
+  #     :admin
+  #   elsif resource.admin == false
+  #     :users
+  #   else
+  #     super
+  #   end
+  # end
 
-  def ActionController
-  end
+  # def ActionController
+  # end
 
 end
